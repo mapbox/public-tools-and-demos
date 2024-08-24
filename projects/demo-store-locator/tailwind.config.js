722f388670cddc5284cde78ae92addc5d4766226
Andrew Sepic <andrew.sepic@mapbox.com>
2024-08-23 20:37:00 -0400

    Fixed custom colors breaking tailwind classNames

--- projects/demo-store-locator/tailwind.config.js
+++ projects/demo-store-locator/tailwind.config.js
@@ -1,25 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 // eslint-disable-next-line
 module.exports = {
-  content: [
-    './index.html',
-    './src/**/*.{js,ts,jsx,tsx,mdx}'
-  ],
+  content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))'
+      },
+      colors: {
+        tintgreen: 'rgba(0, 98, 65, 0.1)',
+        deepgreen: '#006241'
       }
     },
     fontFamily: {
       sans: ['"Open Sans"', 'sans-serif']
-    },
-    colors: {
-      'deepgreen': '#006241',
-      'tintgreen': 'rgba(0, 98, 65, 0.1)'
-    },
+    }
   },
   plugins: []
 }
