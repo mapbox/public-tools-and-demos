18c420f9e9d443177c2d071d449def15fec1c6ea
Andrew Sepic <andrew.sepic@mapbox.com>
2025-05-02 14:10:51 -0400

    Revert map

--- projects/demo-components/src/map.jsx
+++ projects/demo-components/src/map.jsx
@@ -1,117 +1,120 @@
 import PropTypes from 'prop-types'
-import React, { useRef, useEffect, forwardRef } from 'react'
+import React, { useRef, useEffect } from 'react'
 import mapboxgl from 'mapbox-gl'
 import mapboxGeocoder from '@mapbox/mapbox-gl-geocoder'
 
 import 'mapbox-gl/dist/mapbox-gl.css'
 import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css'
+import accessToken from './access-token'
 
-mapboxgl.accessToken = import.meta.env.VITE_YOUR_MAPBOX_ACCESS_TOKEN
+mapboxgl.accessToken = accessToken
 
-const Map = forwardRef(
-  (
-    {
-      center = [0, 0],
-      zoom = 2,
-      style = 'mapbox://styles/mapbox/streets-v12',
-      addGeocoder = false,
-      addNavigationControl = true,
-      onMapLoad,
-      onMapRender,
-      onMapDrag,
-      onMapZoom,
-      onMapRotate,
-      onMapPitch,
-      onMapMove,
-      onMapMoveend,
-      onMapClick,
-      geocoderRef,
-      accessToken,
-      projection = 'globe',
-      hash = false,
-      children
-    },
-    ref
-  ) => {
-    const mapContainer = useRef(null)
-    const internalRef = useRef(null)
-    const mapRef = ref || internalRef
+const Map = (
+  {
+    center = [0, 0],
+    zoom = 2,
+    style = 'mapbox://styles/mapbox/streets-v12',
+    addGeocoder = false,
+    addNavigationControl = true,
+    onMapLoad,
+    onMapRender,
+    onMapDrag,
+    onMapZoom,
+    onMapRotate,
+    onMapPitch,
+    onMapMove,
+    onMapMoveend,
+    onMapClick,
+    geocoderRef,
+    accessToken,
+    projection = 'globe',
+    hash = false,
+    children
+  },
+  ref
+) => {
+  const mapContainer = useRef(null)
 
-    useEffect(() => {
-      if (mapRef.current) {
-        //debugger;
-        if (onMapRender) {
-          mapRef.current.on('load', onMapRender)
-        }
-        return //initialize map once
-      }
-      const map = (mapRef.current = new mapboxgl.Map({
-        container: mapContainer.current,
-        style,
-        center,
-        zoom,
-        projection,
-        accessToken,
-        hash
-      }))
+  let mapRef = ref
 
-      if (addGeocoder) {
-        const geocoder = new mapboxGeocoder({
-          accessToken: mapboxgl.accessToken,
-          mapboxgl: mapboxgl
-        })
-        map.addControl(geocoder)
-
-        if (geocoderRef) {
-          geocoderRef.current = geocoder
-        }
-      }
-
-      if (addNavigationControl) {
-        map.addControl(new mapboxgl.NavigationControl())
-      }
-
-      if (onMapLoad) {
-        map.on('load', onMapLoad)
-      }
-
-      if (onMapDrag) {
-        map.on('drag', onMapDrag)
-      }
-
-      if (onMapZoom) {
-        map.on('zoom', onMapZoom)
-      }
-
-      if (onMapRotate) {
-        map.on('rotate', onMapRotate)
-      }
-
-      if (onMapPitch) {
-        map.on('pitch', onMapPitch)
-      }
-
-      if (onMapMoveend) {
-        map.on('moveend', onMapMoveend)
-      }
-
-      if (onMapMove) {
-        map.on('move', onMapMove)
-      }
-
-      if (onMapClick) {
-        map.on('click', onMapClick)
-      }
-    })
-    return (
-      <div ref={mapContainer} className='map-container h-full'>
-        {children}
-      </div>
-    )
+  /* eslint-disable react-hooks/rules-of-hooks */
+  if (!mapRef) {
+    mapRef = useRef(null)
   }
-)
+  /* eslint-enable react-hooks/rules-of-hooks */
 
-Map.displayName = 'Map'
+  useEffect(() => {
+    if (mapRef.current) {
+      //debugger;
+      if (onMapRender) {
+        mapRef.current.on('load', onMapRender)
+      }
+      return //initialize map once
+    }
+    const map = (mapRef.current = new mapboxgl.Map({
+      container: mapContainer.current,
+      style,
+      center,
+      zoom,
+      projection,
+      accessToken,
+      hash
+    }))
+
+    if (addGeocoder) {
+      const geocoder = new mapboxGeocoder({
+        accessToken: mapboxgl.accessToken,
+        mapboxgl: mapboxgl
+      })
+      map.addControl(geocoder)
+
+      if (geocoderRef) {
+        geocoderRef.current = geocoder
+      }
+    }
+
+    if (addNavigationControl) {
+      map.addControl(new mapboxgl.NavigationControl())
+    }
+
+    if (onMapLoad) {
+      map.on('load', onMapLoad)
+    }
+
+    if (onMapDrag) {
+      map.on('drag', onMapDrag)
+    }
+
+    if (onMapZoom) {
+      map.on('zoom', onMapZoom)
+    }
+
+    if (onMapRotate) {
+      map.on('rotate', onMapRotate)
+    }
+
+    if (onMapPitch) {
+      map.on('pitch', onMapPitch)
+    }
+
+    if (onMapMoveend) {
+      map.on('moveend', onMapMoveend)
+    }
+
+    if (onMapMove) {
+      map.on('move', onMapMove)
+    }
+
+    if (onMapClick) {
+      map.on('click', onMapClick)
+    }
+  })
+  return (
+    <div ref={mapContainer} className='map-container h-full'>
+      {children}
+    </div>
+  )
+}
 
 Map.propTypes = {
   accessToken: PropTypes.string,
