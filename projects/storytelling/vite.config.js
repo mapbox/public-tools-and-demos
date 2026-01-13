2fdbea2505526bebe8da2e830e0dea510717d860
Andrew Sepic <andrew.sepic@mapbox.com>
2026-01-13 11:25:37 -0500

    Added storytelling.html into build

--- projects/storytelling/vite.config.js
+++ projects/storytelling/vite.config.js
@@ -1,8 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
+import { fileURLToPath } from 'url'
+
+const __dirname = fileURLToPath(new URL('.', import.meta.url))
 
 export default defineConfig({
   plugins: [react()],
   base: '/storytelling',
-  envDir: '../'
+  envDir: '../',
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        storytelling: resolve(__dirname, 'storytelling.html')
+      }
+    }
+  }
 })
