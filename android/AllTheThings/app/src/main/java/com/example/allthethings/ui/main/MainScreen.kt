4eca12f44599bd8f28c815c5968f591410d8aed8
Antonio Zugaldia <antonio@mapbox.com>
2026-07-12 09:34:18 +0200

    I can see the globe. Could you make the map animate over 2 seconds from the globe view to a zoomed-in view of Orlando, Florida?

--- android/AllTheThings/app/src/main/java/com/example/allthethings/ui/main/MainScreen.kt
+++ android/AllTheThings/app/src/main/java/com/example/allthethings/ui/main/MainScreen.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
@@ -12,8 +13,10 @@ import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation3.runtime.NavKey
 import com.example.allthethings.data.DefaultDataRepository
 import com.mapbox.geojson.Point
+import com.mapbox.maps.dsl.cameraOptions
 import com.mapbox.maps.extension.compose.MapboxMap
 import com.mapbox.maps.extension.compose.animation.viewport.rememberMapViewportState
+import com.mapbox.maps.plugin.animation.MapAnimationOptions
 
 @Composable
 fun MainScreen(
@@ -37,15 +40,26 @@ fun MainScreen(
 
 @Composable
 internal fun MainScreen(modifier: Modifier = Modifier) {
+  val mapViewportState = rememberMapViewportState {
+    setCameraOptions {
+      center(Point.fromLngLat(-81.38, 28.54))
+      zoom(0.0)
+    }
+  }
+
+  LaunchedEffect(Unit) {
+    mapViewportState.flyTo(
+      cameraOptions {
+        center(Point.fromLngLat(-81.38, 28.54))
+        zoom(11.0)
+      },
+      MapAnimationOptions.mapAnimationOptions { duration(2000) },
+    )
+  }
+
   MapboxMap(
     modifier = modifier.fillMaxSize(),
-    mapViewportState =
-      rememberMapViewportState {
-        setCameraOptions {
-          center(Point.fromLngLat(-81.38, 28.54))
-          zoom(0.0)
-        }
-      },
+    mapViewportState = mapViewportState,
     scaleBar = { ScaleBar(Modifier.statusBarsPadding()) },
     logo = { Logo(Modifier.navigationBarsPadding()) },
     attribution = { Attribution(Modifier.navigationBarsPadding()) },
